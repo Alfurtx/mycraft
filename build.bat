b94354ed91ab39f421bde21ea1c113ae52309c52
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2022-08-06 10:50:56 +0200

    feat: add GLAD library

--- build.bat
+++ build.bat
@@ -5,10 +5,16 @@ setlocal
 set CC=cl
 
 set CFLAGS=/W2 /Zi /nologo /Femycraft.exe
+set CFLAGS=%CFLAGS% /Iw:\mycraft\libs
+set LINKFLAGS=/link opengl32 gdi32 user32 kernel32
 set CFILES=
 
 pushd ".\code"
 for /R %%c in (*.cpp) do call set CFILES=%%c %%CFILES%%
+popd
+
+pushd ".\libs\glad"
+for /R %%c in (*.c) do call set CFILES=%%c %%CFILES%%
 popd
 
 if not exist "bin" mkdir bin
--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include "utils.hpp"
 
+#include <glad/glad.h>
+
 int
 main(void)
 {
